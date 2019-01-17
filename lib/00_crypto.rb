b4f552de67d5a4504c92ab770105b47b12031356
TheMindset <mulezig2014@outlook.fr>
2019-01-17 23:48:18 +0100

    update fichier 00_crypto synthax code

--- lib/00_crypto.rb
+++ lib/00_crypto.rb
@@ -1,34 +1,36 @@
 require 'nokogiri'
 require 'rubygems'
 require 'open-uri'
+require 'pry'
 
 def crypto
-    
-  value = []
+
   currency = []
+  value = []
+  a = []
 
   page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+  page2 = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 
   page.xpath('//a[@class = "link-secondary"]').each do |devise|
     currency << devise.text
   end
 
-# print currency
+  #binding.pry
 
-  page.xpath('//a[@class = "price"]').each do |valeur|
+  page2.xpath('//a[@class = "price"]').each do |valeur|
     value << valeur.text
   end
 
-# print value
+  #binding.pry
 
   values = value.map{|e| e.delete('$').to_f }
-
-  my_hash = Hash[currency.zip(values.map)]
-
-  a = []
+  my_hash = Hash[currency.zip(value.map)]
   my_hash.each {|index| a << {index[0] => index[1]}}
-
-  return a
 end
 
-a = crypto
+#binding.pry
+
+def perform
+  crypto
+end
